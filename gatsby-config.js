b256f2a82d21997e459b9ae2292b709f8aabb085
Rejon <rejon@maximumcrash.com>
2020-06-24 21:48:01 -0400

    Learning GH Pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 const path = require("path")
 
 module.exports = {
-  pathPrefix: "/blackgamedevs",
   siteMetadata: {
     title: `Black Game Developers`,
     siteUrl: 'https://blackgamedevs.com',
